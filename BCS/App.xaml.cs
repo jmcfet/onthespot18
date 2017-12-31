0f2e91196f78b280e04dc1bcfe92835f0a948354
john mcfetridge <jmcfet@bellsouth.net>
2017-12-31 17:45:30 -0500

    start

--- BCS/App.xaml.cs
+++ BCS/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnTheSpot.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace BCS
     /// </summary>
     public partial class App : Application
     {
+     //   public BaseViewModel vm = new BCSandGSSVM(true);
     }
 }
---
+++ DAL/AssemblyAccess.cs
@@ -0,0 +1,44 @@
+﻿// OTSAccess dal = new OTSAccess(StoreConnectionString);
+// dal.GetEmployee(1);
+// string error = string.Empty;
+// System.Collections.ObjectModel.ObservableCollection<Category> cats = dal.GetCats(out error);
+using DAL.EFStuff.Assembly;
+using OnTheSpot.Models;
+using System;
+using System.Linq;
+
+namespace OnTheSpot.Dal
+{
+
+    public class AssemblyAccess
+    {
+        AssemblyEntities1 assemblyEnties;
+        
+        public AssemblyAccess(string connectionstring)
+        {
+            assemblyEnties = new AssemblyEntities1();
+        }
+
+        public AutoSortInfo GetCustomerInfoFromAssembly(string itemID)
+        {
+            AutoSortInfo AssemblyStuff = (from auto in assemblyEnties.AutoSorts
+                        where auto.ArticleCode == itemID
+                        join inv in assemblyEnties.Invoices on auto.InvoiceID equals inv.InvoiceID
+                             select new AutoSortInfo() { CustomerID = auto.CustomerID, InvoiceNumber = inv.InvoiceID, dept = inv.Department, Duedate = auto.DueDate , rfid = auto.RFID, storeid = auto.StoreID}).SingleOrDefault();
+
+
+            return AssemblyStuff;
+        }
+
+        public bool GetInfoFromAssembly(string itemID)
+        {
+            int id = Int32.Parse(itemID);
+            Invoice item = assemblyEnties.Invoices.Where(i => i.OrderID == id).SingleOrDefault();
+            if (item == null)
+                return false;
+            else
+                return true;
+        }
+    }
+}
+
