bb98e5a39d5232d5bfbcf15df9df1f2f671953fc
john mcfetridge <jmcfet@bellsouth.net>
2018-08-27 09:20:40 -0400

    test

--- BCS/Views/ItemImage.xaml.cs
+++ BCS/Views/ItemImage.xaml.cs
@@ -7,12 +7,12 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Input; 
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace OnTheSpot.Views
+namespace OnTheSpot.Views   //
 {
     /// <summary>
     /// Interaction logic for ItemImage.xaml
